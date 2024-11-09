7b2119e04698392ad4c23a5a0da41b24d86f2359
Keanu Interone <keanu.interone@gmail.com>
2024-11-09 17:41:08 +1100

    Updated response headers for cors

--- src/AWS/APIGatewayFunctions.py
+++ src/AWS/APIGatewayFunctions.py
@@ -4,7 +4,10 @@ def create_api_gateway_response(status_code, body):
     return {
         "statusCode": status_code,
         "headers": {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",  # Allow requests from any origin
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",  # Allow these headers
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"  # Allow specific methods
         },
         "body": json.dumps(body)
     }
