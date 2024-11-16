594358eaf4ed0797c6f76d782ced10c96ad508a2
Keanu Interone <keanu.interone@gmail.com>
2024-11-16 11:22:33 +1100

    formated messages on context response

--- src/RequestHandlers/GetContextHandler.py
+++ src/RequestHandlers/GetContextHandler.py
@@ -1,9 +1,21 @@
 from Agent.Context import get_context_for_user, create_context
 from Agent.Agent import get_agent_for_user
 
+def transform_context_for_frontend(context):
+    messages = []
+    for message in context["messages"]:
+        if (message["type"] == "human" or message["type"] == "ai"):
+            messages.append({
+                "from": message["type"],
+                "message": message["content"]
+            })
+    context["messages"] = messages
+    return context
+
 def get_context_handler(context_id, agent_id, user_id):        
     if (context_id):
-        return get_context_for_user(context_id, user_id)
+        context = get_context_for_user(context_id, user_id)
+        return transform_context_for_frontend(context)
     
     if (agent_id == None):
         agent_id = "default-agent-1"
@@ -12,7 +24,7 @@ def get_context_handler(context_id, agent_id, user_id):
 
     context = create_context(agent["agent_id"], user_id)
 
-    return context
+    return transform_context_for_frontend(context)
 
 
     
