53e4f9b7e29ab94003661cf30669836a685fa655
Keanu Interone <keanu.interone@gmail.com>
2024-11-18 17:35:57 +1100

    Create organization working

--- src/RequestHandlers/CreateOrganizationHandler.py
+++ src/RequestHandlers/CreateOrganizationHandler.py
@@ -4,6 +4,6 @@ from Models.User import associate_organization_with_user
 def create_organization_handler(name: str, user_id: str) -> dict:
     # Create the organization
     organization = create_organization(name)
-    organization = associate_organization_with_user(organization["organization_id"], user_id)
-    associate_user_with_organization(user_id, organization["organization_id"])
+    organization = associate_user_with_organization(organization["org_id"], user_id)
+    associate_organization_with_user(user_id, organization["org_id"])
     return organization
